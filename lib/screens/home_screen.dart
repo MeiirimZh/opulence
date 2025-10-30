8c985f19c776b14aecfdaf6b6c46ea88de5e2b4a
MeiirimZh <zh.meyirim@gmail.com>
2025-10-30 12:31:03 +0500

    added table for incomes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,43 +11,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("hello");
+            },
+            icon: SvgPicture.asset('assets/icons/calendar.svg', width: 24, height: 24)
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Количество нажатий на кнопку:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(color: Colors.black54, width: 2),
-          borderRadius: BorderRadius.circular(100),
-        ),
-        child: const Icon(Icons.add),
-      ),
+      body: Padding(padding: EdgeInsets.only(left: 20, top: 50), child:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 10.0,
+          children: [
+          Text('Доходы', style: TextStyle(
+            fontSize: 16,
+          )),
+          Table(
+            border: TableBorder.all(color: Colors.black),
+            columnWidths: const <int, TableColumnWidth>{
+              0: FixedColumnWidth(60.0),
+              1: FixedColumnWidth(100.0),
+              2: FixedColumnWidth(190.0),
+            },
+            children: [
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('Время'))),
+                  TableCell(child: Center(child: Text('Доход'))),
+                  TableCell(child: Center(child: Text('Источник')))
+                ]
+              ),
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('11:45'))),
+                  TableCell(child: Center(child: Text('10 000'))),
+                  TableCell(child: Center(child: Text('Lorem')))
+              ]),
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('19:23'))),
+                  TableCell(child: Center(child: Text('5 000'))),
+                  TableCell(child: Center(child: Text('Ipsum')))
+              ])
+            ],
+          )
+        ],))
     );
   }
 }
