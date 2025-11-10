b8f597e3eea0b5356cbbe39b27d42bcc6a0229bd
MeiirimZh <zh.meyirim@gmail.com>
2025-11-10 09:13:31 +0500

    added form

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,13 +13,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  // Контролеры для получения текста
+  final _sumController = TextEditingController();
+
+  @override
+  void dispose() {
+    _sumController.dispose();
+    super.dispose();
+  }
+
   String _msg = "";
+  String _inputAction = "+";
 
   void _setMsg(String newMsg) {
     setState(() {
       _msg = newMsg;
       saveData();
     });
+  }
+
+  void _setInputAction() {
+    setState(() {
+      if (_inputAction == "+") {
+        _inputAction = "−";
+      } else {
+        _inputAction = "+";
+      }
+    });
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final sum = _sumController.text;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Введенная сумма: $sum')),
+      );
+    }
   }
 
   Future<void> saveData() async {
@@ -73,102 +104,137 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.only(left: 20, top: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 10.0,
-          children: [
-            Text('Доходы', style: TextStyle(fontSize: 16)),
-            Table(
-              border: TableBorder.all(color: Colors.black),
-              columnWidths: const <int, TableColumnWidth>{
-                0: FixedColumnWidth(60.0),
-                1: FixedColumnWidth(100.0),
-                2: FixedColumnWidth(190.0),
-              },
-              children: [
-                TableRow(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, top: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 10.0,
+            children: [
+              Text('Доходы', style: TextStyle(fontSize: 16)),
+              Table(
+                border: TableBorder.all(color: Colors.black),
+                columnWidths: const <int, TableColumnWidth>{
+                  0: FixedColumnWidth(60.0),
+                  1: FixedColumnWidth(100.0),
+                  2: FixedColumnWidth(190.0),
+                },
+                children: [
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('Время'))),
+                      TableCell(child: Center(child: Text('Доход'))),
+                      TableCell(child: Center(child: Text('Источник'))),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('11:45'))),
+                      TableCell(child: Center(child: Text('10 000'))),
+                      TableCell(child: Center(child: Text('Lorem'))),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('19:23'))),
+                      TableCell(child: Center(child: Text('5 000'))),
+                      TableCell(child: Center(child: Text('Ipsum'))),
+                    ],
+                  ),
+                ],
+              ),
+              Text('Расходы', style: TextStyle(fontSize: 16)),
+              Table(
+                border: TableBorder.all(color: Colors.black),
+                columnWidths: const <int, TableColumnWidth>{
+                  0: FixedColumnWidth(60.0),
+                  1: FixedColumnWidth(100.0),
+                  2: FixedColumnWidth(190.0),
+                },
+                children: [
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('Время'))),
+                      TableCell(child: Center(child: Text('Расход'))),
+                      TableCell(child: Center(child: Text('Источник'))),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('12:16'))),
+                      TableCell(child: Center(child: Text('1 000'))),
+                      TableCell(child: Center(child: Text('Lorem'))),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('14:42'))),
+                      TableCell(child: Center(child: Text('2000'))),
+                      TableCell(child: Center(child: Text('Ipsum'))),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      TableCell(child: Center(child: Text('20:01'))),
+                      TableCell(child: Center(child: Text('300'))),
+                      TableCell(child: Center(child: Text('Dolor'))),
+                    ],
+                  ),
+                ],
+              ),
+              // Text(_msg, style: TextStyle(fontSize: 18.0)),
+              // Row(
+              //   spacing: 10.0,
+              //   children: [
+              //     ElevatedButton(
+              //       onPressed: () => _setMsg("Привет, Lorem!"),
+              //       child: Text('Lorem'),
+              //     ),
+              //     ElevatedButton(
+              //       onPressed: () => _setMsg("Привет, Ipsum!"),
+              //       child: Text('Ipsum'),
+              //     ),
+              //     ElevatedButton(
+              //       onPressed: () => _setMsg("Привет, Dolor!"),
+              //       child: Text('Dolor'),
+              //     ),
+              //   ],
+              // ),
+              Form(
+                key: _formKey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
                   children: [
-                    TableCell(child: Center(child: Text('Время'))),
-                    TableCell(child: Center(child: Text('Доход'))),
-                    TableCell(child: Center(child: Text('Источник'))),
+                    ElevatedButton(
+                      onPressed: () => _setInputAction(),
+                      child: Text(_inputAction),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _sumController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Сумма',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Поле обязательно для заполнения';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _submitForm(),
+                      child: Text(">"),
+                    ),
                   ],
                 ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('11:45'))),
-                    TableCell(child: Center(child: Text('10 000'))),
-                    TableCell(child: Center(child: Text('Lorem'))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('19:23'))),
-                    TableCell(child: Center(child: Text('5 000'))),
-                    TableCell(child: Center(child: Text('Ipsum'))),
-                  ],
-                ),
-              ],
-            ),
-            Text('Расходы', style: TextStyle(fontSize: 16)),
-            Table(
-              border: TableBorder.all(color: Colors.black),
-              columnWidths: const <int, TableColumnWidth>{
-                0: FixedColumnWidth(60.0),
-                1: FixedColumnWidth(100.0),
-                2: FixedColumnWidth(190.0),
-              },
-              children: [
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('Время'))),
-                    TableCell(child: Center(child: Text('Расход'))),
-                    TableCell(child: Center(child: Text('Источник'))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('12:16'))),
-                    TableCell(child: Center(child: Text('1 000'))),
-                    TableCell(child: Center(child: Text('Lorem'))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('14:42'))),
-                    TableCell(child: Center(child: Text('2000'))),
-                    TableCell(child: Center(child: Text('Ipsum'))),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('20:01'))),
-                    TableCell(child: Center(child: Text('300'))),
-                    TableCell(child: Center(child: Text('Dolor'))),
-                  ],
-                ),
-              ],
-            ),
-            Text(_msg, style: TextStyle(fontSize: 18.0)),
-            Row(
-              spacing: 10.0,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _setMsg("Привет, Lorem!"),
-                  child: Text('Lorem'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setMsg("Привет, Ipsum!"),
-                  child: Text('Ipsum'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setMsg("Привет, Dolor!"),
-                  child: Text('Dolor'),
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
